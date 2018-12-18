7a492116d6989326aedb8bbaff2e5f8e6148efb4
Alex Macdonald <almacdon@redhat.com>
2018-12-18 17:00:12 -0500

    place cursor at location 0, 0 after setting up the RCP Application display

--- scripts/3_run_ui_tests.sh
+++ scripts/3_run_ui_tests.sh
@@ -5,14 +5,14 @@ JMC_ROOT=$JMC_QA/jmc
 JMC_CORE=$JMC_ROOT/core
 JMC_THIRD_PARTY=$JMC_ROOT/releng/third-party
 JMC_CONSOLE_UITEST_DIR=$JMC_ROOT/application/uitests/org.openjdk.jmc.console.uitest
-MBeansTest_intermittentMBeanTest=$JMC_CONSOLE_UITEST_DIR/src/test/java/org/openjdk/jmc/console/uitest/MBeansTest.java
+RCP_APPLICATION=$JMC_ROOT/application/org.openjdk.jmc.rcp.application/src/main/java/org/openjdk/jmc/rcp/application/Application.java
 
 # run the jetty server in the background
 mvn jetty:run -f $JMC_THIRD_PARTY/pom.xml &
 jetty_pid=$!;
 
-# comment out failing test
-sed -i '125,150 s/^/\/\//' $MBeansTest_intermittentMBeanTest
+# add a cursor placement for 0, 0 in the RCP application setup
+sed -i '53 i 		display.setCursorLocation(0, 0);' $RCP_APPLICATION
 
 # run ui tests
 mvn verify -P uitests -Dspotbugs.skip=true -f $JMC_ROOT || { exit 1; };
