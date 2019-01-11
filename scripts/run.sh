3c901c552e0a38990b0f043ee221f2898492ed90
Alex Macdonald <almacdon@redhat.com>
2019-01-11 16:42:39 -0500

    add exit conditions to run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Clone the JMC repo, build jmc-core and jmc
-bash $(dirname "$0")/1_setup_jmc.sh
+bash $(dirname "$0")/1_setup_jmc.sh || { echo "Failed to build JMC."; exit 1; };
 
 # Clone the Jemmy repo, build it, and move the resulting jars into jmc
-bash $(dirname "$0")/2_setup_jemmy.sh
+bash $(dirname "$0")/2_setup_jemmy.sh || { echo "Failed to setup Jemmy."; exit 1; };
 
 # Run the ui tests
-bash $(dirname "$0")/3_run_ui_tests.sh
+bash $(dirname "$0")/3_run_ui_tests.sh || { echo "Failed to run UI tests."; exit 1; };
