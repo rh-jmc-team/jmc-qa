a74063259bf4874feda43ae01d9c19068e5ca885
Alex Macdonald <almacdon@redhat.com>
2019-01-11 16:42:32 -0500

    remove sed to fix ui test now that JMC-6329 is merged

--- scripts/3_run_ui_tests.sh
+++ scripts/3_run_ui_tests.sh
@@ -10,11 +10,6 @@ jetty_pid=$!;
 # Add a cursor placement for 0, 0 in the RCP application setup.
 sed -i '53 i 		display.setCursorLocation(0, 0);' $RCP_APPLICATION_JAVA
 
-# Addresses https://github.com/aptmac/jmc-qa/issues/22
-# MBeanBrowserTabTest.testValueFontSize() asserts that a font can be resized to the system default height.
-# However, the test uses a hardcoded value of 11 to test against, and the default for Travis & Jenkins is 10.
-sed -i 's/DEFAULT_FONT_HEIGHT = 11/DEFAULT_FONT_HEIGHT = JFaceResources.getDefaultFont().getFontData()[0].getHeight()/' $MBEAN_BROWSER_TAB_TEST
-
 # run ui tests
 mvn verify -P uitests -Dspotbugs.skip=true -f $JMC_ROOT/pom.xml || { kill $jetty_pid; exit 1; };
 
